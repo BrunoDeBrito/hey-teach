af3f919d0cd519f88a10682e4d9bcd84bf0751b1
BrunoDeBrito <brunordebrito@gmail.com>
2026-03-01 15:10:10 -0300

    refactoring to create an action method on user model to like a question

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -7,7 +7,7 @@ use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\{RedirectResponse, Request};
 use Illuminate\Support\Facades\{Hash, Password};
 use Illuminate\Support\Str;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\{Rules, ValidationException};
 use Illuminate\View\View;
 
 class NewPasswordController extends Controller
@@ -23,7 +23,7 @@ class NewPasswordController extends Controller
     /**
      * Handle an incoming new password request.
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
     public function store(Request $request): RedirectResponse
     {
