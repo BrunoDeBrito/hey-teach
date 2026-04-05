ce6a48d871c24861f5d82aa542997893863dd816
Bruno DeBrito <brunordebrito@gmail.com>
2026-04-05 14:27:45 -0300

    remove ideia

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * @method static withSum(string $string, string $string1)
- */
 class Question extends Model
 {
     use HasFactory;
