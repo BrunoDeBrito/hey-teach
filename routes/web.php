56bda44ac00362989f92e4ab5880542e09c2c451
BrunoDeBrito <brunordebrito@gmail.com>
2026-04-15 11:47:48 -0300

    ajustes finos

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', DashboardController::class)
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::prefix('question')->name('question.')->group(function () {
-    Route::post('store', [QuestionController::class, 'store'])->name('store');
-    Route::post('like/{question}', Question\LikeController::class)->name('like');
-    Route::post('unlike/{question}', Question\UnlikeController::class)->name('unlike');
-});
+Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::prefix('profile')->name('profile.')->group(function () {
-        Route::get('/', [ProfileController::class, 'edit'])
-                ->name('edit');
-        Route::patch('/', [ProfileController::class, 'update'])
-            ->name('update');
-        Route::delete('/', [ProfileController::class, 'destroy'])
-            ->name('destroy');
-    });
+    #region Question Routes
+    Route::get('/question', [QuestionController::class, 'index'])->name('question.index');
+    Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
+    Route::delete('/question/{question}', [QuestionController::class, 'destroy'])->name('question.destroy');
+    Route::post('/question/like/{question}', Question\LikeController::class)->name('question.like');
+    Route::post('/question/unlike/{question}', Question\UnlikeController::class)->name('question.unlike');
+    Route::put('/question/publish/{question}', Question\PublishController::class)->name('question.publish');
+    #endregion
+
+    #region Profile Routes
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    #endregion
 });
 
 require __DIR__ . '/auth.php';
